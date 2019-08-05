6676086410f2ca0de31162953d891fd9fae97fd5
gmzelias <gmzelias@gmail.com>
2019-08-05 17:14:50 -0400

    New logos and USD calculator

--- js/main.js
+++ js/main.js
@@ -37,47 +37,63 @@ function showIt2() {
 
 jQuery.ajax({
     type: "GET",
-    url: 'http://ec2-18-237-86-164.us-west-2.compute.amazonaws.com:3000/API/rates',
-    //url:'http://localhost:3000/API/rates',
+    //url: 'http://ec2-18-237-86-164.us-west-2.compute.amazonaws.com:3000/API/rates',
+    url:'http://localhost:3000/API/rates',
     rejectUnauthorized: false,
     requestCert: true, 
     agent: false,
     success: function(res) { 
         var strVES = res.vesDashRate;
-
+        var strUSD = res.usdDashRate;
         //************************************** 1MM, 10MM, 100MM***********************************//START
         if(strVES.length === 9){
             var mainBsRate = strVES.replace(".", ",");
             mainBsRate = mainBsRate.slice(0,3) + "." + mainBsRate.slice(3);
-            $("#mainDashRate").text(mainBsRate + ' Bs');
+            $("#mainDashRate").text(mainBsRate + ' Bs' + ' = ' + strUSD +' USD');
         } 
         if(strVES.length === 10){
             var mainBsRate = strVES.replace(".", ",");
             mainBsRate = mainBsRate.slice(0,1) + "." + mainBsRate.slice(1,4)  + "." + mainBsRate.slice(4);
-           $("#mainDashRate").text(mainBsRate + ' Bs');
+            $("#mainDashRate").text(mainBsRate + ' Bs' + ' = ' + strUSD +' USD');
         } 
         if(strVES.length === 11){
             var mainBsRate = strVES.replace(".", ",");
             mainBsRate = mainBsRate.slice(0,2) + "." + mainBsRate.slice(2,5)  + "." + mainBsRate.slice(5);
-           $("#mainDashRate").text(mainBsRate + ' Bs');
+            $("#mainDashRate").text(mainBsRate + ' Bs' + ' = ' + strUSD +' USD');
         } 
         if(strVES.length === 12){
             var mainBsRate = strVES.replace(".", ",");
             mainBsRate = mainBsRate.slice(0,3) + "." + mainBsRate.slice(3,6)  + "." + mainBsRate.slice(6);
-           $("#mainDashRate").text(mainBsRate + ' Bs');
+            $("#mainDashRate").text(mainBsRate + ' Bs' + ' = ' + strUSD +' USD');
         } 
         //************************************** 1MM, 10MM, 100MM***********************************//END
+
+        console.log($("#dashRate").val());
 
         $('#bsRate').bind("keyup change",function(e){
             var dashCalculation = ($("#bsRate").val() / strVES).toFixed(8);
             $("#dashRate").val(dashCalculation);
+            var USDCalculationforBs = ($("#dashRate").val() * strUSD).toFixed(2);
+            $("#usdRate").val(USDCalculationforBs);
         });
         $('#dashRate').bind("keyup change",function(e){
             var bsCalculation = ($("#dashRate").val() * strVES).toFixed(2);
             $("#bsRate").val(bsCalculation);
+            var bsCalculationforUSD = ($("#dashRate").val() * strUSD).toFixed(2);
+            $("#usdRate").val(bsCalculationforUSD);
         });
+
+        $('#usdRate').bind("keyup change",function(e){
+            var dashCalculationFromUSD = ($("#usdRate").val() / strUSD).toFixed(8);
+            $("#dashRate").val(dashCalculationFromUSD);
+            var bsCalculation = ($("#dashRate").val() * strVES).toFixed(2);
+            $("#bsRate").val(bsCalculation);
+        });
+
         setTimeout("showIt2()", 180000); // after 3 mins
         $("#bsRate").val(strVES);
+        $("#usdRate").val(strUSD);
+        $("#dashRate").val(1);
         $("#loaderSpinner").hide();
         $("#apiDocs").show();
         $("#mainBox").fadeIn("slow");  
